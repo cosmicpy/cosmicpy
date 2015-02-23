86ec835443cf6f26c7c9ada60b9a417f4d01941b
Francois Lanusse <francois.lanusse@cea.fr>
2015-02-23 23:43:57 +0100

    Improved Travis compilation script

---
+++ .deploy_doc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git config user.name "CosmicPy Travis CI Worker"
+git config user.email "travis@cosmicpy.github.io"
+git clone git@github.com:cosmicpy/cosmicpy.github.io.git
+
+(cd cosmicpy.github.io
+	cp -r $HOME/build/cosmicpy/cosmicpy/doc/build/html/* .
+	git add *
+	git commit -m "Deployed documentation from Travis"
+	git push --quiet git@github.com:cosmicpy/cosmicpy.github.io.git > /dev/null 2>&1
+)
+rm -rf cosmicpy.github.io
